5f0b7d053322b6426120b1994ef71141d38a3ec2
Luke Jones <luke.jones@modulis.ca>
2016-07-15 12:31:23 -0400

    Add configuration and convenience methods
    You can now configure DIDWW and check whether it's in live mode or
    sandbox mode using convenience methods

--- lib/DIDWW.rb
+++ lib/DIDWW.rb
@@ -1,5 +1,32 @@
 require "DIDWW/version"
+require "DIDWW/configuration"
 
 module DIDWW
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
+
+  def self.reset
+    @configuration = Configuration.new
+  end
+
+  def self.mode
+    configuration.sandbox ? :sandbox : :live
+  end
+
+  def self.sandbox?
+    configuration.sandbox
+  end
+
+  def self.live?
+    !configuration.sandbox
+  end
 end
